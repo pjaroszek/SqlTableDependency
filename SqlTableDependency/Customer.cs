493109e316280a6b7aed863c24b19c9c448c2157
Paweł Jaroszek <pawel.jaroszek@digit-al.pl>
2019-11-15 10:38:50 +0100

    the project is working properly

---
+++ SqlTableDependency/Customer.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System;
+
+namespace Jaroszek.ProofOfConcept.SqlTableDependency
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+
+
+        public string City { get; set; }
+        public DateTime Born { get; set; }
+    }
+}
